d5eff7d3bc65fe37962c2ecc0dc962bd4755f40b
Kentaro Takiguchi <takiguchi0817@gmail.com>
2017-08-13 15:19:05 +0100

    Split adapters

---
+++ app/src/main/java/rejasupotaro/arxiv/reader/ui/paper/list/PaperListAdapter.kt
@@ -0,0 +1,39 @@
+package rejasupotaro.arxiv.reader.ui.paper.list
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.list_item_search_result.view.*
+import rejasupotaro.arxiv.reader.R
+import rejasupotaro.arxiv.reader.data.model.Paper
+
+class PaperListAdapter(
+        val onItemClickListener: (Paper) -> Unit,
+        val onItemLongClickListener: (Paper) -> Boolean = { _ -> false }
+) : RecyclerView.Adapter<PaperViewHolder>() {
+    var items = mutableListOf<Paper>()
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: PaperViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PaperViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item_paper, parent, false)
+        return PaperViewHolder(itemView, onItemClickListener, onItemLongClickListener)
+    }
+}
+
+class PaperViewHolder(
+        itemView: View,
+        val onItemClickListener: (Paper) -> Unit,
+        val onItemLongClickListener: (Paper) -> Boolean
+) : RecyclerView.ViewHolder(itemView) {
+    fun bind(paper: Paper) {
+        itemView.titleTextView.text = paper.title
+        itemView.setOnClickListener { onItemClickListener.invoke(paper) }
+        itemView.setOnLongClickListener { onItemLongClickListener.invoke(paper) }
+    }
+}
+
--- app/src/main/java/rejasupotaro/arxiv/reader/ui/paper/list/PaperListFragment.kt
+++ app/src/main/java/rejasupotaro/arxiv/reader/ui/paper/list/PaperListFragment.kt
@@ -11,11 +11,10 @@ import kotlinx.android.synthetic.main.fragment_paper_list.*
 import rejasupotaro.arxiv.reader.R
 import rejasupotaro.arxiv.reader.data.model.Paper
 import rejasupotaro.arxiv.reader.ui.common.NavigationController
-import rejasupotaro.arxiv.reader.ui.paper.search.SearchResultListAdapter
 
 class PaperListFragment : LifecycleFragment() {
     private val viewModel = PaperListViewModel()
-    lateinit var adapter: SearchResultListAdapter
+    lateinit var adapter: PaperListAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_paper_list, container, false)
@@ -31,7 +30,7 @@ class PaperListFragment : LifecycleFragment() {
     }
 
     private fun setupListView() {
-        adapter = SearchResultListAdapter(
+        adapter = PaperListAdapter(
                 onItemClickListener = { paper ->
                     NavigationController.navigateToViewer(paper)
                 },
