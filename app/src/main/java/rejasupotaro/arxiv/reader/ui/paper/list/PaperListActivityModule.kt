2a5a0a99fe710a5221b7e994a35c0c51228f15eb
Kentaro Takiguchi <takiguchi0817@gmail.com>
2017-08-20 20:35:40 +0100

    Inject Activity

--- app/src/main/java/rejasupotaro/arxiv/reader/ui/paper/list/PaperListActivityModule.kt
+++ app/src/main/java/rejasupotaro/arxiv/reader/ui/paper/list/PaperListActivityModule.kt
@@ -1,6 +1,8 @@
 package rejasupotaro.arxiv.reader.ui.paper.list
 
+import android.app.Activity
 import android.content.Context
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import rejasupotaro.arxiv.reader.data.repository.PaperRepository
@@ -8,7 +10,12 @@ import rejasupotaro.arxiv.reader.data.repository.PaperRepository
 @Module
 class PaperListActivityModule {
     @Provides
-    fun providePaperListViewModel(context: Context, paperRepository: PaperRepository): PaperListViewModel {
-        return PaperListViewModel(context, paperRepository)
+    fun provideActivity(activity: PaperListActivity): Activity {
+        return activity
+    }
+
+    @Provides
+    fun providePaperListViewModel(activity: Activity, paperRepository: PaperRepository): PaperListViewModel {
+        return PaperListViewModel(activity, paperRepository)
     }
 }
--- app/src/main/java/rejasupotaro/arxiv/reader/ui/paper/list/PaperListViewModel.kt
+++ app/src/main/java/rejasupotaro/arxiv/reader/ui/paper/list/PaperListViewModel.kt
@@ -1,18 +1,18 @@
 package rejasupotaro.arxiv.reader.ui.paper.list
 
+import android.app.Activity
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.ViewModel
-import android.content.Context
 import rejasupotaro.arxiv.reader.OpenClassOnDebug
 import rejasupotaro.arxiv.reader.data.file.FileManager
 import rejasupotaro.arxiv.reader.data.model.Paper
 import rejasupotaro.arxiv.reader.data.repository.PaperRepository
 
 @OpenClassOnDebug
-class PaperListViewModel(private val context: Context, private val paperRepository: PaperRepository) : ViewModel() {
+class PaperListViewModel(private val activity: Activity, private val paperRepository: PaperRepository) : ViewModel() {
     fun paperList(): LiveData<List<Paper>> = paperRepository.list()
     fun deletePaper(paper: Paper): LiveData<Unit> {
-        val file = FileManager.paperToFile(context, paper)
+        val file = FileManager.paperToFile(activity, paper)
         return paperRepository.delete(paper to file)
     }
 }
