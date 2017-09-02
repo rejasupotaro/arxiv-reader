623c9868bd7345fd2ebba0206e8ecfafb58cdc1c
rejasupotaro <takiguchi0817@gmail.com>
2017-09-02 07:38:43 +0100

    Add index

--- app/src/main/java/rejasupotaro/arxiv/reader/data/db/Migrations.kt
+++ app/src/main/java/rejasupotaro/arxiv/reader/data/db/Migrations.kt
@@ -6,8 +6,8 @@ import android.arch.persistence.room.migration.Migration
 fun migrations(): Array<Migration> = arrayOf(AddPaperSimilaritiesTable())
 
 class AddPaperSimilaritiesTable : Migration(1, 2) {
-    override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("""
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("""
             CREATE TABLE IF NOT EXISTS `paper_similarities` (
                 `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                 `from_paper_id` INTEGER NOT NULL,
@@ -17,6 +17,7 @@ class AddPaperSimilaritiesTable : Migration(1, 2) {
                 FOREIGN KEY(`to_paper_id`) REFERENCES `papers`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE
             )
             """)
+        db.execSQL("CREATE UNIQUE INDEX `index_paper_similarities_from_paper_id_to_paper_id` ON `paper_similarities` (`from_paper_id`, `to_paper_id`)")
     }
 }
 
